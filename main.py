2858d33e159da55b1b75535d66d6295f2e4849f2
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2024-10-03 08:07:42 +0545

    modifited code to show sql query

--- main.py
+++ main.py
@@ -2,11 +2,37 @@ from langchain_helper import get_few_shot_db_chain
 
 import streamlit as st
 
-st.title("NepTech Retail Store: Database Q&A  💻")
+st.title("NepTech Retail Store: Database 💻")
 
-question = st.text_input("Question: ")
-if question:
+if 'question' not in st.session_state:
+    st.session_state.question = ""
+with st.form(key='my_form'):
+    question = st.text_input("Question:", value=st.session_state.question)
+    col1, col2 = st.columns(2)
+
+    # Submit button in the first column
+    with col1:
+        submit_button = st.form_submit_button(label='Submit')
+
+    # Clear button in the second column
+    with col2:
+        clear_button = st.form_submit_button(label='Clear')
+
+if clear_button:
+    st.session_state.question = ""
+
+if submit_button and question:
     chain = get_few_shot_db_chain()
-    answer = chain.run(question)
+    result = chain.invoke(question)
+    #answer = chain.run(question)
+    answer = result['result']
     st.header("Answer:")
-    st.write(answer)
+    st.code(answer,language='')
+
+    intermediate_steps = result["intermediate_steps"]
+    sql_query = intermediate_steps[2]['sql_cmd'] if 'sql_cmd' in intermediate_steps[2] else intermediate_steps[1]
+    # Display the SQL query and the final answer
+    st.header("Generated SQL Query:")
+    st.code(sql_query, language='sql')
+    
+    
